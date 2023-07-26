f7bd504167cfa4e4352efbe53ac74a4c53e03aa7
Tiago Lima <tiago.lima@oli.live>
2023-07-26 15:56:51 -0300

    fix: responsive to ios

--- src/components/EmptyScreen/index.tsx
+++ src/components/EmptyScreen/index.tsx
@@ -2,7 +2,7 @@ import EmptyImage from '@/assets/empty_screen.svg';
 
 export const EmptyScreen = () => {
   return (
-    <div className="grid h-full w-full place-items-center mt-20">
+    <div className="grid w-full place-items-center mt-20">
       <EmptyImage />
       <h1 className="text-white mb-2">Don't know what to search?</h1>
       <p className="text-gray">Here's an offer you can't refuse</p>
