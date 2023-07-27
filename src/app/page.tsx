e024f13fc9064be02613d2767a439a273dfa50da
Tiago Lima <tiago.lima@oli.live>
2023-07-27 19:19:44 -0300

    feat: add suggestion of movie to search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
           {movies?.allMoviedetaileds?.length ? (
             <MoviesList movies={movies?.allMoviedetaileds} />
           ) : (
-            <EmptyScreen />
+            <EmptyScreen setSearchMovies={setSearchMovies} />
           )}
         </>
       )}
--- src/components/EmptyScreen/index.tsx
+++ src/components/EmptyScreen/index.tsx
@@ -1,11 +1,23 @@
+import { Dispatch, SetStateAction } from 'react';
+
 import EmptyImage from '@/assets/EmptyScreen.svg';
 
-export const EmptyScreen = () => {
+type EmptyScreenProps = {
+  setSearchMovies: Dispatch<SetStateAction<string>>;
+};
+
+export const EmptyScreen = ({ setSearchMovies }: EmptyScreenProps) => {
   return (
     <div className="grid w-full place-items-center mt-20">
       <EmptyImage />
       <h1 className="text-white mb-2">Don't know what to search?</h1>
       <p className="text-gray">Here's an offer you can't refuse</p>
+      <button
+        className="text-white bg-[#f5f5f51f] p-4 rounded-lg my-6"
+        onClick={() => setSearchMovies('harry potter')}
+      >
+        Harry Potter
+      </button>
     </div>
   );
 };
